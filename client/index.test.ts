8d47ba1abaacfff8b7f229694ce7fffa5ad596e6
0xDev05 <divpatel1274@gmail.com>
2025-07-06 01:20:27 +0530

    testing done

---
+++ client/index.test.ts
@@ -0,0 +1,77 @@
+import {test , expect} from "bun:test";
+import { LiteSVM } from "litesvm";
+import {
+	PublicKey,
+	Transaction,
+	SystemProgram,
+	Keypair,
+	LAMPORTS_PER_SOL,
+    TransactionInstruction
+} from "@solana/web3.js";
+
+
+
+//we'dy've payer
+//now we have to create data accont for the counter program
+test("one transfer", () => {
+	const svm = new LiteSVM();
+	const payer = new Keypair();
+	svm.airdrop(payer.publicKey, BigInt(LAMPORTS_PER_SOL));
+	const program_id = PublicKey.unique();
+    svm.addProgramFromFile(program_id,"./CPI.so");
+    const data_account = new Keypair();
+
+	const blockhash = svm.latestBlockhash();
+	const transferLamports = 1_000_000n;
+	const ixs = [
+		SystemProgram.createAccount({
+            fromPubkey:payer.publicKey,
+            newAccountPubkey:data_account.publicKey,
+            lamports:Number(svm.minimumBalanceForRentExemption(BigInt(4))),
+            space: 4,
+            programId:program_id
+        })
+	];
+
+	const tx = new Transaction();
+	tx.recentBlockhash = blockhash;
+	tx.add(...ixs);
+	tx.sign(payer , data_account);
+	svm.sendTransaction(tx);
+    console.log("Account info: ", svm.getAccount(data_account.publicKey));
+
+
+    const ix2 = [new TransactionInstruction({
+        keys: [
+          {pubkey: data_account.publicKey, isSigner: false, isWritable: true},
+        ],
+        programId: program_id,
+        data:Buffer.from(""),
+    }),{
+        keys: [
+          {pubkey: data_account.publicKey, isSigner: false, isWritable: true},
+        ],
+        programId: program_id,
+        data:Buffer.from(""),
+    },{
+        keys: [
+          {pubkey: data_account.publicKey, isSigner: false, isWritable: true},
+        ],
+        programId: program_id,
+        data:Buffer.from(""),
+    },{
+        keys: [
+          {pubkey: data_account.publicKey, isSigner: false, isWritable: true},
+        ],
+        programId: program_id,
+        data:Buffer.from(""),
+    }];
+    const tx2 = new Transaction();
+	tx2.recentBlockhash = blockhash;
+    tx2.feePayer = payer.publicKey;
+	tx2.add(...ix2);
+	tx2.sign(payer);
+	svm.sendTransaction(tx2);
+    console.log("Account info: ", svm.getAccount(data_account.publicKey));
+    
+});
